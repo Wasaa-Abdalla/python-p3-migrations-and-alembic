7891e73e3483858d2ec0280342f4182a569ed2cc
abdalla-wasaa <abdallawasaa7@gmail.com>
2024-02-13 12:38:49 +0300

    Attempted

---
+++ lib/migrations/versions/3b05eb90b7ef_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 3b05eb90b7ef
+Revises: 
+Create Date: 2024-02-13 12:34:03.838367
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3b05eb90b7ef'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
